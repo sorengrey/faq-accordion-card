ae2ec943246963760edabf5ebaf8b4674c26db3f
Heather Smith <halexsmith86@gmail.com>
2021-07-15 16:38:46 -0400

    added gradient bg and dropdown arrows

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("test");
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-btn')) {
+      var dropdowns = document.getElementsByClassName("answer");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
