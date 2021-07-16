17007f0b56ae5b0b500c6d2f509cf006f320a5ce
Heather Smith <halexsmith86@gmail.com>
2021-07-16 16:40:00 -0400

    started setting up js functions, aligned arrows

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 console.log("test");
 
-function dropDownArrow() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.drop-btn')) {
-      var dropdowns = document.getElementsByClassName("answer");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+// changes arrow image to up or down when clicked
+function flipArrow() {
+    var dropbtn = document.querySelector("drop-btn");
+    dropbtn.setAttribute("class", "rotated");
+}
+
+// shows the hidden answer section when arrow is clicked (maybe just add this to flipArrow?)
+function unhideAnswer(){}
+
+// close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".drop-btn")) {
+    var dropdowns = document.getElementsByClassName("answer");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
