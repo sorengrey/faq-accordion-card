f940963a0bd75f4e9046027b7e64a866f0c1f3f0
Heather Smith <halexsmith86@gmail.com>
2021-07-17 22:35:55 -0400

    drop button arrows rotate, bolds questions, shows answers

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-console.log("test");
-
-// changes arrow image to up or down when clicked
-function flipArrow() {
-    var dropbtn = document.querySelector("drop-btn");
+// hides and unhides answers, rotates arrows when clicked
+function clickArrow() {
+    var dropbtn = document.querySelector(".drop-btn");
     dropbtn.setAttribute("class", "rotated");
+
+    var question = document.querySelector(".question");
+    question.setAttribute("class", "bold");
+
+    var answer = document.querySelector(".answer");
+    answer.setAttribute("class", ".answer-show");
+    
+    // if the drop button's class is already .rotated, remove the class and hide the answer - this doesn't work yet?
+    if(dropbtn.classList.contains("rotated")){
+       dropbtn.removeClass("rotated");
+    }
 }
 
-// shows the hidden answer section when arrow is clicked (maybe just add this to flipArrow?)
-function unhideAnswer(){}
 
 // close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".drop-btn")) {
-    var dropdowns = document.getElementsByClassName("answer");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+// window.onclick = function (event) {
+//   if (!event.target.matches(".drop-btn")) {
+//     var dropdowns = document.getElementsByClassName("answer");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
