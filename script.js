859a4e90107d5dcfcac3e8528db2afe78683d3b2
Heather Smith <halexsmith86@gmail.com>
2021-07-19 15:57:19 -0400

    fixed bold answer text issue

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // selects the dropdown section and questions
 let dropDown = document.querySelectorAll(".dropdown .question");
 
-// takes in the click event, shows answers, bolds questions, rotates arrows when clicked, does the opposite when clicked again
+// takes in the click event - shows answers, bolds questions, rotates arrows when clicked, does the opposite when clicked again
 let clickArrow = (e) => {
 var dropbtn = e.target;
 var question = e.currentTarget.parentNode;
-var answer = e.currentTarget.nextElementSibling;
+var answer = e.currentTarget.nextElementSibling;;
 
 if(answer.style.display === "none") {
     answer.style.display = "block";
+    answer.style.fontWeight = "400";
     question.style.fontWeight = "700";
     dropbtn.style.transform = "rotate(180deg)";
 } else {
